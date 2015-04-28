0bf33ae76970dba976e780392563dfa7442cbda4
osupake <epak811@gmail.com>
2015-04-28 16:23:19 -0400

    worked on multiplication table

--- multtable.php
+++ multtable.php
@@ -6,9 +6,41 @@
 		echo "Minimum multiplier is larger than maximum.";
 	}
 
-	$height = $_GET["max-multiplicand"] - $_GET["min-multiplicand"] + 2;
-	$width = $_GET["max-multiplier"] - $_GET["min-multiplier"] + 2;
+	$height = $_GET["max-multiplicand"];
+	$width = $_GET["max-multiplier"];
 
-	var_dump($height);
-	var_dump($width);
+	//var_dump($height);
+	//var_dump($width);
+
+	$height_start = $_GET["min-multiplicand"];
+	$width_start = $_GET["min-multiplier"];
+
+	echo("<table>");
+	echo('<th width="50px"></th>');
+
+	while($width_start <= $width) {
+		echo '<th width="50px" align="left">';
+		echo $width_start;
+		echo "</th>";
+		$width_start++;
+	}
+	echo "<br>";
+
+	while($height_start <= $height) {
+		echo "<tr>";
+		echo "<th>";
+		echo $height_start;
+		echo "</th>";
+
+		$width_start = $_GET["min-multiplier"];
+		while($width_start <= $width) {
+			$value = $height_start * $width_start;
+			echo '<td width="50px">';
+			echo $value;
+			echo "</td>";
+			$width_start++;
+		}
+		$height_start++;
+		echo "</tr>";
+	}
 ?>
