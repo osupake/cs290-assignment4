0e10e9522a1c6b1f404815d988fee36dfcd660f1
osupake <epak811@gmail.com>
2015-04-28 20:33:33 -0400

    started login.php. added feature to reject null or empty usernames

---
+++ login.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="main.css">
+	<title>Eugene Pak | CS290 Assignment 4</title>
+</head>
+<body>
+	<div class="formContainer">
+		<form action="content1.php" method="post">
+			<fieldset>
+				<ul>
+					<li><label for="username">Username</label><input type="text" name="username" id="username"></li>
+				</ul>
+				<input type="submit" value="Login">
+			</fieldset>
+		</form>
+	</div>	
+</body>
+</html>
--- loopback.php
+++ loopback.php
@@ -1,9 +1,13 @@
 <?php
 	$type = $_SERVER['REQUEST_METHOD'];
+	//$counter = count($_GET);
+	//echo $counter;
+	//print_r($_GET);
+	$parameters = null;
 
-	if($type == 'POST') {
+	if($type == 'POST' && !empty($_POST)) {
 		$parameters = $_POST;
-	} else if($type == 'GET') {
+	} else if($type == 'GET' && !empty($_GET)) {
 		$parameters = $_GET;
 	}
 
